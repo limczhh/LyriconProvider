2713ba7d000d793b855fd35d64132a0877c6293d
lidesheng <169997481+limczhh@users.noreply.github.com>
2026-08-19 00:15:25 +0800

    chore: 限制 ABI 为 arm64-v8a

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -28,6 +28,10 @@ configure<ApplicationExtension> {
         versionName = "1.0.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+
+        ndk {
+            abiFilters += "arm64-v8a"
+        }
     }
 
     signingConfigs {
